4f607d23a49ae62fca5ce244785b1fb760a4685a
Amir <amirshaipov199@gmail.com>
2026-09-17 15:16:19 +0300

    Codeforces <<Too long words>>, took me an hour

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,7 +1,27 @@
-#include "libs.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <memory>
+#include <random>
 
 using namespace std;
 
 int main() {
+    int amm, leng;
+    cin >> amm;
+    vector<string> v(amm);
+    for (int i = 0; i < amm; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < amm; i++) {
+        leng = v[i].size();
+        if (leng > 10) {
+            cout << v[i][0] << leng - 2 << v[i][leng - 1] << "\n";
+        }
+        else {
+            cout << v[i] << "\n";
+        }
+    }
     return 0;
 }
