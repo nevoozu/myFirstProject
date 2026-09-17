397efc78944a0a25d00d7dab4e35cbfcbe361bc0
Amir <amirshaipov199@gmail.com>
2026-09-17 17:20:06 +0300

    "Next round" on Codeforces. Took me around 20 minutes

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -8,10 +8,18 @@
 using namespace std;
 
 int main() {
-    int amm, x, y, dN;
-    cin >> x >> y;
-    amm = x * y;
-    dN = amm / 2;
-    cout << dN << "\n";
+    int n, k,amm = 0;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i ++) {
+        cin >> v[i];
+    }
+    int gb = v[k - 1];
+    for (int i = 0; i < n; i++) {
+        if (v[i] >= gb && v[i] > 0) {
+            amm += 1;
+        }
+    }
+    cout << amm << "\n";
     return 0;
 }
