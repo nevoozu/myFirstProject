7b099a7e97e7fc527813192d6d5de2ab3fa13323
Amir <amirshaipov199@gmail.com>
2026-09-18 17:08:38 +0300

    "Capitalize" on Codeforces

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -4,9 +4,14 @@
 #include <string>
 #include <memory>
 #include <random>
+#include <cctype>
 
 using namespace std;
 
 int main() {
+    string str;
+    cin >> str;
+    str[0] = std::toupper(static_cast<unsigned char>(str[0]));
+    cout << str;
     return 0;
 }
